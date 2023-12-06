669aa132007822325b1083c101f49092b637c2c7
ilija <ilijapet@gmail.com>
2023-12-06 11:07:52 +0100

    bringin in jwt branch

--- backend/musicNFT/urls.py
+++ backend/musicNFT/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 logger = logging.getLogger(__name__)
 logger.info("Loading urls.py")
@@ -11,6 +12,6 @@ logger.info("Loading urls.py")
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", TemplateView.as_view(template_name="home.html")),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
-
-# ilija
