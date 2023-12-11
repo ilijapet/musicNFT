faff81cf3cf40fab469467381d1484d73b35f0b7
ilija <ilijapet@gmail.com>
2023-12-11 10:48:23 +0100

    updatets

--- backend/musicNFT/settings/__init__.py
+++ backend/musicNFT/settings/__init__.py
@@ -10,6 +10,8 @@ ENVVAR_SETTINGS_PREFIX = "BACKENDSETTINGS_"
 # 1) This will be false
 LOCAL_SETTINGS_PATH = os.getenv(f"{ENVVAR_SETTINGS_PREFIX}LOCAL_SETTINGS_PATH")
 
+# INside docker-compose.yml file used in prod there is local settings path local/settings.prod.py'
+# What means in prodcution this dev settings will not be loaded
 if not LOCAL_SETTINGS_PATH:
     # 2) that is why this will be true. What means assigne dev settings
     LOCAL_SETTINGS_PATH = "local/settings.dev.py"
